b24ae494f8356a09f79cf7fac7169e7f22001f9b
Alissonrf <alisson.flores@paytrack.com.br>
2022-09-07 18:42:44 -0300

    Primeiro component criado

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,55 @@
+const repositoryName = "unform2";
+
+
+export function RepositoryList() {
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositorios</h1>
+
+            
+
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar respositorio
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar respositorio
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar respositorio
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar respositorio
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar respositorio
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
