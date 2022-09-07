086b1e9bff683be8dfec43844ff893092368caf8
Alissonrf <alisson.flores@paytrack.com.br>
2022-09-07 19:09:52 -0300

    utilizando useState

---
+++ src/components/Contador.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+// hook 
+
+export function Counter() {
+
+    const [counter, setCounter] = useState(0);
+
+    function increment(){
+        setCounter(counter + 1);
+    }
+
+
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button type="button" onClick={increment}>increment</button>
+        </div>
+    )
+}
