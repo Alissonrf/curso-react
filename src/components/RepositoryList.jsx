3ca5f804e7b15dac50d07d38c2659d4734bf261f
Alissonrf <alisson.flores@paytrack.com.br>
2022-09-07 19:51:56 -0300

    fazendo uma chamada http para o github via api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,30 @@
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-    name: "unform",
-    description: "Forms in react",
-    link: "https://github.com/unform/unform"
-}
+import '../styles/repositories.scss'
+import { useState, useEffect} from "react";
+
+// https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([])
+    
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
+    console.log(repositories)
+
     return(
         <section className="repository-list">
             <h1>Lista de repositorios</h1>
 
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>
+                })}
             </ul>
         </section>
     )
